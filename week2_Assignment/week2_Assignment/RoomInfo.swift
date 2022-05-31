8ec74daea05bfdf4f36a37bf37421cb4e3c96fa9
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-31 18:10:12 +0300

    Create RoomInfo struct

---
+++ week2_Assignment/week2_Assignment/RoomInfo.swift
@@ -0,0 +1,14 @@
+//
+//  RoomInfo.swift
+//  week2_Assignment
+//
+//  Created by Furkan Açıkgöz on 31.05.2022.
+//
+
+import Foundation
+import UIKit
+
+struct RoomInfo {
+    var name: String
+    var image: UIImage
+}
