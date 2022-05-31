d14d247897eb0f016e2d101b1cac67b60494e3dd
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-31 17:53:00 +0300

    Create Xcode project

---
+++ week2_Assignment/week2_Assignment/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week2_Assignment
+//
+//  Created by Furkan Açıkgöz on 31.05.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
