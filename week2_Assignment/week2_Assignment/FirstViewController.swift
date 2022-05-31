e1a4a09f6f946affbb1879ec123d85909c0718c9
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-31 18:37:19 +0300

    Add image assets and createData method to FirstVC

--- week2_Assignment/week2_Assignment/FirstViewController.swift
+++ week2_Assignment/week2_Assignment/FirstViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 class FirstViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var roomsInfo: [RoomInfo] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Initialize the VC as delegate of table view
+        // Initializing the VC as delegate of table view
         tableView.delegate = self
+        
+        // Creating data
+        createData()
     }
+}
+
+extension FirstViewController {
+    
+    func createData() {
+        roomsInfo.append(RoomInfo(name: "Living Room", image: UIImage(named: "livingRoom")!))
+        roomsInfo.append(RoomInfo(name: "Kitchen", image: UIImage(named: "kitchen")!))
+        roomsInfo.append(RoomInfo(name: "Bedroom", image: UIImage(named: "bedroom")!))
+        roomsInfo.append(RoomInfo(name: "Bathroom", image: UIImage(named: "bathroom")!))
+        roomsInfo.append(RoomInfo(name: "Gaming Room", image: UIImage(named: "gameRoom")!))
+    }
+    
 }
 
 extension FirstViewController: UITableViewDelegate {
