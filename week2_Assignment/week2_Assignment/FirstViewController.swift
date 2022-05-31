c3420dafb4f47f5d13365f5df0f4182cf4fca78d
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-31 18:07:13 +0300

    Add Table View to FirstVC and conform to the Table View Delegate

--- week2_Assignment/week2_Assignment/FirstViewController.swift
+++ week2_Assignment/week2_Assignment/FirstViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Initialize the VC as delegate of table view
+        tableView.delegate = self
     }
-
-
 }
 
+extension FirstViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension FirstViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+}
