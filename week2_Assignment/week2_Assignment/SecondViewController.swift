1a6bd57b34bb903cac1606ae5b353dbe474326a3
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-06-01 15:09:52 +0300

    Create SecondVC and connect to UI elements

---
+++ week2_Assignment/week2_Assignment/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  week2_Assignment
+//
+//  Created by Furkan Açıkgöz on 1.06.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
